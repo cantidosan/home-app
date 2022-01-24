32b1d0c4958f5473a0bf5e37199ee7d81227a6e7
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-01-24 17:00:14 +0000

    implement flex properties

--- pages/about.js
+++ pages/about.js
@@ -7,18 +7,58 @@ export default function About() {
 
     return (
 
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <div className={styles.nameBlock}>
+        <div className="flex flex-row bg-teal-500 justify-between">
+            <div className="flex-col text-left bg-orange-500">
+                <div className="font-medium">
                     Name:
                     Gavin B
 
                 </div>
             </div>
-            <div className={styles.right}>
-                <div className={styles.LogoBlock}>
+            <div className="flex flex-row grow flex-wrap justify-between bg-olive-500">
+                <div className="">
 
-                    Javascript,Node,Express,Sass,Postgres,Mongo,NextJs
+                    Javascript
+
+                </div>
+                <div className="">
+
+                    NextJs
+
+                </div>
+                <div className="">
+
+                    Node
+
+                </div>
+                <div className="">
+
+                    Tailwind
+
+                </div>
+                <div className="">
+
+                    Express
+
+                </div>
+                <div className="">
+
+                    Sass
+
+                </div>
+                <div className="">
+
+                    Reactjs
+
+                </div>
+                <div className="">
+
+                    Postgres
+
+                </div>
+                <div className="">
+
+                    Mongo
 
                 </div>
             </div>
