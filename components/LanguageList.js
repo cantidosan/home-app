f2c709ef4c4c97f34a2971bbefa743ec77cc6b2a
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-01-25 17:25:28 +0000

    refactored Languages as React Components

---
+++ components/LanguageList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import LanguageInfo from './LanguageInfo';
+export default function LanguageList(props) {
+
+    const languages = [
+        { label: "Javascript", href: "" },
+        { label: "Nextjs", href: "" },
+        { label: "Nodejs", href: "" },
+        { label: "Tailwind", href: "" },
+        { label: "Express", href: "" },
+        { label: "Sass", href: "" },
+        { label: "Reactjs", href: "" },
+        { label: "Postgres", href: "" },
+        { label: "MongoDB", href: "" }];
+
+    return (
+        <div className="flex basis-1/2  gap-4 flex-row grow flex-wrap justify-between bg-teal-500 ">
+            {languages.map((lang, key) => (
+
+                <LanguageInfo lang={lang} key={key} />
+
+            ))}
+        </div>
+    );
+}
--- pages/about.js
+++ pages/about.js
@@ -1,67 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import LanguageInfo from '../components/LanguageInfo';
+import LanguageList from '../components/LanguageList';
 import styles from '../styles/about.module.css'
 
 
 export default function About() {
 
+
+
     return (
 
-        <div className="flex flex-row bg-teal-500 justify-between">
-            <div className="flex-col text-left bg-orange-500">
+        <div className="flex flex-row  justify-between">
+            <div className="flex-col basis-1/2 text-left bg-cyan-400">
                 <div className="font-medium">
                     Name:
                     Gavin B
+                    <p>
+                        lorem  ipsum floatsum jetsum,Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
 
                 </div>
             </div>
-            <div className="flex flex-row grow flex-wrap justify-between bg-olive-500">
-                <div className="">
+            <LanguageList />
 
-                    Javascript
 
-                </div>
-                <div className="">
-
-                    NextJs
-
-                </div>
-                <div className="">
-
-                    Node
-
-                </div>
-                <div className="">
-
-                    Tailwind
-
-                </div>
-                <div className="">
-
-                    Express
-
-                </div>
-                <div className="">
-
-                    Sass
-
-                </div>
-                <div className="">
-
-                    Reactjs
-
-                </div>
-                <div className="">
-
-                    Postgres
-
-                </div>
-                <div className="">
-
-                    Mongo
-
-                </div>
-            </div>
         </div>
 
     );
